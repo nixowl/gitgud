46b625c69fb13f7abd153ad00bc0096d7494d501
Luka <lukabrkovic@skiff.com>
2024-02-10 15:58:38 +0100

    feat: icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,29 @@
-import { ThemeToggle } from "@/components/ui/themetoggle";
-import Image from "next/image";
+import Image from "next/image"
+import { Icons } from "@/components/icons"
+import { ThemeToggle } from "@/components/ui/themetoggle"
 
 export default function Home() {
   return (
     <>
-      <header className="px-4 py-2 flex items-center justify-center bg-background">
+      <header className="flex items-center justify-center bg-background px-4 py-2">
         <p className="">header</p>
       </header>
-      <main className="flex-1 flex items-center justify-center mx-auto">
+      <main className="mx-auto flex flex-1 items-center justify-center">
         <p className="text-accent-foreground">main</p>
       </main>
-      <footer className="px-4 py-2 flex justify-between bg-background">
+      <footer className="flex justify-between bg-background px-4 py-2">
         <div className="z-50 flex size-6 items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
           <div className="block sm:hidden">xs</div>
-          <div className="hidden sm:block md:hidden lg:hidden xl:hidden 2xl:hidden">
-            sm
-          </div>
-          <div className="hidden md:block lg:hidden xl:hidden 2xl:hidden">
-            md
-          </div>
+          <div className="hidden sm:block md:hidden lg:hidden xl:hidden 2xl:hidden">sm</div>
+          <div className="hidden md:block lg:hidden xl:hidden 2xl:hidden">md</div>
           <div className="hidden lg:block xl:hidden 2xl:hidden">lg</div>
           <div className="hidden xl:block 2xl:hidden">xl</div>
           <div className="hidden 2xl:block">2xl</div>
         </div>
+        <Icons.Attachment />
 
         <ThemeToggle />
       </footer>
     </>
-  );
+  )
 }
