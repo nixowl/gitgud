0adf183da3a02e28493f186cbd4570a64ff198d4
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-02-10 14:31:43 +0100

    initial commit

---
+++ src/components/ui/themetoggle.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
+
+export function ThemeToggle() {
+  const { setTheme, theme } = useTheme();
+
+  function toggleTheme() {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
+
+  return (
+    <Button
+      onClick={toggleTheme}
+      variant="ghost"
+      size="icon"
+      className="bg-transparent hover:bg-transparent hover:text-accent"
+    >
+      {theme === "dark" ? (
+        <Sun className="h-5 w-5" />
+      ) : (
+        <Moon className="h-5 w-5" />
+      )}
+    </Button>
+  );
+}
