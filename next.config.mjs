a02aa2dfcfb764b8f94b852930e64e53fade8d5c
Luka <brkovicluka1997@gmail.com>
2024-02-18 01:50:53 +0100

    drop: app router

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+}
 
 export default nextConfig
+
